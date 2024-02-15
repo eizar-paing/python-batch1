dc1314b4711bbe52c95fb633c6424cb6dc3170be
Susan2570 <sunandaraung.conceptx@gmail.com>
2024-02-15 12:17:02 +0630

    ex_to_fun

--- homeworks/ex_to_fun.py
+++ homeworks/ex_to_fun.py
@@ -1,32 +1,32 @@
-# exercises to function
-# for nested loop
-def nested_loop(row_num): # can't put user_row here, u will have to give new name
-    for i in range(1, row_num + 1):
-        for j in range(i):
-            print(i, end = "")
-        print()
+# # exercises to function
+# # for nested loop
+# def nested_loop(row_num): # can't put user_row here, u will have to give new name
+#     for i in range(1, row_num + 1):
+#         for j in range(i):
+#             print(i, end = "")
+#         print()
 
-user_row = int(input("Enter your row number: "))
-nested_loop(user_row)
+# user_row = int(input("Enter your row number: "))
+# nested_loop(user_row)
 
 
-# *  *  *  *
-# *        *
-# *        *
-# *  *  *  *
+# # *  *  *  *
+# # *        *
+# # *        *
+# # *  *  *  *
 
-# for square_star
-def square_star(a_b):
-    for i in range(a_b):
-        for j in range(a_b):
-            if i == 0 or i == a_b - 1 or j == 0 or j == a_b - 1:
-                print("*", end = " ")
-            else:
-                print(" ", end = " ")
-        print()
+# # for square_star
+# def square_star(a_b):
+#     for i in range(a_b):
+#         for j in range(a_b):
+#             if i == 0 or i == a_b - 1 or j == 0 or j == a_b - 1:
+#                 print("*", end = " ")
+#             else:
+#                 print(" ", end = " ")
+#         print()
 
-user_input = int(input("Enter your row number: "))
-square_star(user_input)
+# user_input = int(input("Enter your row number: "))
+# square_star(user_input)
 
 # Trace
 # user will input her value in user_input
@@ -70,4 +70,16 @@ square_star(user_input)
 #           inner loop ==> 3
 #           inner loop ==> 4
 
+# new one
+def my_color(my_list):
+    print(f"my fav color is {my_list[user_fav]}")
 
+list_list = []
+
+for i in range(5): # 0 to 4
+    user_list = input("Enter your colors: ")
+    list_list.append(user_list)
+
+print(list_list)
+user_fav = int(input("Enter your fav colors index: "))
+my_color(list_list)
