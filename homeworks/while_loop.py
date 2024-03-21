e2310650a29aa13dc4c94f3e23f7a2bc3fcd2134
Susan2570 <sunandaraung.conceptx@gmail.com>
2024-03-21 12:47:51 +0630

    while_loop

--- homeworks/while_loop.py
+++ homeworks/while_loop.py
@@ -5,12 +5,58 @@
 # Eg. print(n, 'is a even number') or print(n, 'is a even number')
 #  5.   Check how many times a given number can be divided by 3 before it is less than or equal to 10.
 
-# for 1
+# # for 1
+# print ("No.1")
+# username = "Susan"
+# password = "564789sv"
 
-username = "Susan"
-password = "564789sv"
+# while True:
+#     user_username = input("Enter your username: ")
+#     user_password = input("Enter your password: ")
 
-user_username = input("Enter your username: ")
-user_password = input("Enter your oassword: ")
+#     if user_username == username and user_password == password:
+#         print("Welcome user!")
+#         break
+#     else: 
+#         print("Fail")
+
+# # for 2
+# print("No.2")
+
+# while True:
+#     user_num = int(input("Enter your number: "))
+
+#     if user_num > 100 and user_num < 500:
+#         print("Well done!")
+#         break
+#     else:
+#         print("Try again")
+
+# # for 3
+# print("No.3")
+
+# character = "Susan2Aung0"
+# total = 0
+
+# while total < len(character): # 0 < 11:
+#     i = character[total] # i = S
+#     if i.isdigit(): # i is dight?, No
+#         break 
+#     print(i) # S
+#     total += 1 # 1 = 0 + 1
+
+# for 4
+print("No.4")
+
+user_number = int(input("Enter ypur number: "))
+pass
+
+    
+
+
+
+
+
+    
 
 
