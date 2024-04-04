ff82cb21e46e9f1f12aa69ea3bfbf3f2998ff38c
eizar-paing <eizarpaing.sg@gmail.com>
2024-04-04 17:59:30 +0630

    tkinter calculator functions

--- projects/calculator.py
+++ projects/calculator.py
@@ -5,6 +5,30 @@ def getDigit(digit):
   new = current + str(digit)
   result_label.config(text=new)
 
+def clear():
+  result_label.config(text='')
+
+def getOperator(op):
+  global number1, operator
+  number1 = int(result_label['text'])
+  operator = op
+  result_label.config(text='')
+
+def getResult():
+  number2 = int(result_label['text'])
+  if operator == '+':
+    result = number1 + number2
+  elif operator == '-':
+    result = number1 - number2
+  elif operator == '*':
+    result = number1 * number2
+  else:
+    if number2 == 0:
+      result = 'Error'
+    else:
+      result = number1 / number2
+  result_label.config(text=str(result))
+
 root = Tk()
 root.title("Calculator")
 root.geometry('350x400')
@@ -15,11 +39,11 @@ result_label = Label(root, text='', bg='black', fg='white')
 result_label.grid(row=0, column=0, columnspan=4, pady=(40, 20))
 result_label.config(font=('Verdana', 32, 'bold'))
 
-btn7 = Button(root, text='7', width=5, height=2, command=lambda :getDigit(7))
+btn7 = Button(root, text='7', width=5, height=2, command= lambda :getDigit(7))
 btn7.grid(row=1, column=0)
 btn7.config(font=('Verdana', 14))
 
-btn8 = Button(root, text='8', width=5, height=2, command=lambda :getDigit(8))
+btn8 = Button(root, text='8', width=5, height=2, command= lambda :getDigit(8))
 btn8.grid(row=1, column=1)
 btn8.config(font=('Verdana', 14))
 
@@ -27,7 +51,7 @@ btn9 = Button(root, text='9', width=5, height=2, command=lambda :getDigit(9))
 btn9.grid(row=1, column=2)
 btn9.config(font=('Verdana', 14))
 
-btn_add = Button(root, text='+', width=5, height=2)
+btn_add = Button(root, text='+', width=5, height=2, command=lambda :getOperator('+'))
 btn_add.grid(row=1, column=3)
 btn_add.config(font=('Verdana', 14))
 
@@ -43,7 +67,7 @@ btn6 = Button(root, text='6', width=5, height=2, command=lambda :getDigit(6))
 btn6.grid(row=2, column=2)
 btn6.config(font=('Verdana', 14))
 
-btn_sub = Button(root, text='-', width=5, height=2)
+btn_sub = Button(root, text='-', width=5, height=2, command=lambda :getOperator('-'))
 btn_sub.grid(row=2, column=3)
 btn_sub.config(font=('Verdana', 14))
 
@@ -60,7 +84,7 @@ btn3 = Button(root, text='3', width=5, height=2, command=lambda :getDigit(3))
 btn3.grid(row=3, column=2)
 btn3.config(font=('Verdana', 14))
 
-btn_mul = Button(root, text='*', width=5, height=2)
+btn_mul = Button(root, text='*', width=5, height=2, command=lambda :getOperator('*'))
 btn_mul.grid(row=3, column=3)
 btn_mul.config(font=('Verdana', 14))
 
@@ -68,15 +92,15 @@ btn0 = Button(root, text='0', width=5, height=2, command=lambda :getDigit(0))
 btn0.grid(row=4, column=0)
 btn0.config(font=('Verdana', 14))
 
-btn_clear = Button(root, text='C', width=5, height=2)
+btn_clear = Button(root, text='C', width=5, height=2, command=lambda :clear())
 btn_clear.grid(row=4, column=1)
 btn_clear.config(font=('Verdana', 14))
 
-btn_equal = Button(root, text='=', width=5, height=2)
+btn_equal = Button(root, text='=', width=5, height=2, command=lambda :getResult())
 btn_equal.grid(row=4, column=2)
 btn_equal.config(font=('Verdana', 14))
 
-btn_div = Button(root, text='/', width=5, height=2)
+btn_div = Button(root, text='/', width=5, height=2, command=lambda :getOperator('/'))
 btn_div.grid(row=4, column=3)
 btn_div.config(font=('Verdana', 14))
 
